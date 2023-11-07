4cc14c90c32102be296b48ef1775119c004fa2fe
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-07 15:27:44 +0600

    delete request created to delete blog from wistlist

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -58,6 +58,13 @@ async function run() {
         const query = {'user.email':email};
         const cursor = wishlistCollection.find(query);
         const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.delete('/wishlist/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await blogCollection.deleteOne(query)
         res.send(result)
     })
 
